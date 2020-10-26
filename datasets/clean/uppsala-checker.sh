90aece678f86274b671de35444fe7429cc9bd302
Theodor Blom <theodor.blom1@gmail.com>
2020-10-26 21:28:37 +0100

    Finished script for checking for NaN values

--- datasets/clean/uppsala-checker.sh
+++ datasets/clean/uppsala-checker.sh
@@ -9,15 +9,34 @@ if [[ "xDATAINPUT" == "x" ]]; then
     exit 1
 fi
 
-DATALOC=$(dirname "$DATAINPUT")
-
-DATAFILE=$(basename "$DATAINPUT")
-
+## Find the number of occurances of each year and make sure it's either 365 or 366
 echo "Years with missing dates:"
-for (( i = 1722; i <= 2013; i++ )) ; do
+for (( i = 1722; i <= 2013; i++ )); do
     DAYS=$( grep -c "${i}" $DATAINPUT )
-    if [[ "$DAYS" != @(365|366) ]] ; then
+    if [[ "$DAYS" != @(365|366) ]]; then
         printf "${i}: "
         grep -c "${i}" $DATAINPUT
     fi 
 done
+
+NUMLINES=$( wc -l $DATAINPUT | awk -F' ' '{print $1}' )
+
+## Our data is in the fourth column
+DATA=$( cat $DATAINPUT | awk -F' ' '{print $4}' ) 
+
+echo "Checking if all ${NUMLINES} lines of data are numbers."
+for (( j = 1; j < $NUMLINES; j++ )); do
+    DATALINE=$( echo $DATA | awk -F' ' '{print $'${j}'}' )
+
+    if (( $j%100 == 0 )); then
+        echo "Successfully checked ${j} out of ${NUMLINES} values."
+    fi
+
+    re='^[+-]?[0-9]?[0-9]?([.][0-9]+)?$'
+    if ! [[ $DATALINE =~ $re ]]; then
+        echo "error on line ${j}: Not a number" >&2; exit 1
+    fi
+done
+
+echo "Successfully checked all ${NUMLINES} values."
+
