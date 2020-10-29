97aed0d7d94b7368e6ab13c61d5f8a27b415300b
Theodor Blom <theodor.blom1@gmail.com>
2020-10-29 23:07:57 +0100

    Started adding some basic functionality to the project code. Constructors, destructors and some get/set functions for the filepath.

--- code/tempTrender.cpp
+++ code/tempTrender.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
 #include "tempTrender.h"
 
-tempTrender::tempTrender(string filePath) {
-	cout << "The user supplied " << filePath << " as the path to the data file." << endl;
+tempTrender::tempTrender(std::string filePath) {
+    std::cout << "The user supplied " << filePath << " as the path to the data file." << std::endl;
+    tempTrender::setFilePath(filePath);
+}
+
+void tempTrender::setFilePath(std::string filePath) {
+    pFilePath = filePath;
+}
+
+std::string tempTrender::getFilePath() {
+    return pFilePath;
+}
+
+void tempTrender::tempPerDay() {
+    
+    // Uppsala data only has average temperatures
+    // If we don't find uppsala in filepath we must do some calculations first.
+    std::string uppsala = "uppsala";
+    if (pFilePath.find(uppsala) == std::string::npos) {
+        std::cout << "We didn't find uppsala!" << std::endl;
+
+        // Here we calculate the average temperature for each day. (If not uppsala data)
+    }
+
+    // Here we can start plotting the average temperature for each day (For uppsala we already have this)
+    
 }
 
 
