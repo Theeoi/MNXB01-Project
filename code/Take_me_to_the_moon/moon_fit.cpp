5702edc09334c42f9e0146b2b492336a628c23e6
Justinas Smertinas <justinas.smertinas@gmail.com>
2020-11-05 22:19:27 +0100

    Made changes so that we have coloured graphs.

--- code/Take_me_to_the_moon/moon_fit.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-// Lets try and read the CSV.
-auto df = ROOT::RDF::MakeCsvDataFrame("final_file.csv");
-TLinearFitter *lf=new TLinearFitter(3);
-lf ->SetFormula("x ++ y ++ z");
-
