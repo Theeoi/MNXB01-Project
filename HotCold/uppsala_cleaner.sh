a6ab6c1f18c5bd82f7c1f2757e037e63063ecede
Sara Wildenor <sara.wildenor@hotmail.com>
2020-11-06 10:40:34 +0100

    file for data cleaning

---
+++ HotCold/uppsala_cleaner.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+usage(){
+	echo "----"
+	echo -e "  To call this script please use"
+	echo -e "   $0 <path>"
+	echo -e "  Examples:"
+        echo -e "   $0 uppsala_tm_1722-2013.dat"
+	echo "----"
+}
+
+SMHIINPUT=$1
+
+if [[ x$SMHIINPUT == x ]]; then
+   echo "Missing input file parameter, exiting";
+   usage
+   exit 1
+fi
+
+DATAFILE=$(basename $SMHIINPUT)
+
+cp -a $SMHIINPUT original_$DATAFILE
+
+
+if [[ $? != 0 ]]; then
+   echo "Error downloading or copying file, maybe wrong command syntax? exiting....";
+   usage
+   exit 1
+fi
+
+#cut -d' ' -f1-4 original_$DATAFILE | 
+
+sed 's/      /     /g' original_$DATAFILE | sed 's/     /    /g' | sed 's/    /   /g' | sed 's/   /  /g' | sed 's/  / /g' > clean_$DATAFILE
+
+awk '{if ($6 == 1) print $1, $2, $3, $4}' clean_$DATAFILE > hotcold_uppsala.csv
+#| awk '{ if ($2 == 04) print $1, $2, $3, $4}' | awk '{ if ($3 == 30) print $1, $2, $3, $4}' | sed 's/ /,/g' > VALBORG_$DATAFILE
+
+# clean2_$DATAFILE > valborg_uppsala.csv
+
+#cut -d',' -f1-4 
+#cut -d' ' -f1-1 clean_$DATAFILE | sed 's/-//g' > date_$DATAFILE
+#cut -d' ' -f3- clean_$DATAFILE > temp_$DATAFILE
+
+
+
+
