23cadbfef649f766e775fb7f61f3bd8fc7f4f4b9
Theodor Blom <theodor.blom1@gmail.com>
2020-11-09 20:00:01 +0100

    Restored my lost code :(

--- code/project.cpp
+++ code/project.cpp
@@ -19,8 +19,8 @@ void project() {
 
     // END of Battle for Valborg!
     
-    std::string pathToFile = "../datasets/uppsala_tm_1722-2013.dat"; //Put the path to your data file here
-	tempTrender t(pathToFile); //Instantiate your analysis object
+    //std::string pathToFile = "../datasets/uppsala_tm_1722-2013.dat"; //Put the path to your data file here
+	//tempTrender t(pathToFile); //Instantiate your analysis object
 	
 	//t.tempOnDay(8, 23); //Call some functions that you've implemented
 	//t.tempOnDay(235);
